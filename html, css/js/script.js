6b36247cd8c0fbc366077d1e5980367c328f4596
Barbara <barbara.lopez@ikasle.egibie.org>
2021-09-29 08:54:58 +0200

    actualización

--- html, css/js/script.js
+++ html, css/js/script.js
@@ -1,7 +1,14 @@
+/*
+     var pequeñoReso=window.matchMedia("(max-width: 315px)");
+     if(pequeñoReso.matches){
+        document.getElementById("titulo").style.display = "none";
+    }
+*/
 function actionNav(){
     var mobileReso = window.matchMedia("(max-width: 550px)")
     var tabletReso = window.matchMedia("(max-width: 768px)")
     var desktopReso = window.matchMedia("(min-width: 768px)")
+   
 
     if(document.getElementById("sidenav").style.display == "none"){
         if(mobileReso.matches){
@@ -21,7 +28,8 @@ function actionNav(){
         document.getElementById("sidenav").style.width = "0";
         document.getElementById("sidenav").style.display = "none";
         document.getElementById("main").style.width = "100%";
-        document.getElementById("main").style.paddingLeft = "2.2em";
+        document.getElementById("main").style.paddingLeft = "3em";
 
     }
+    
 }
