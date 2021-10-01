83015c5f5333d62cea1a204a4c35d2631e36e025
unai <unai.diez@ikasle.egibide.org>
2021-10-01 12:24:28 +0200

    animacion full auto + pause v2
    animacion con opcion a pararlo

--- html, css/js/animaciontren.js
+++ html, css/js/animaciontren.js
@@ -1,17 +1,18 @@
 
-function animaciones(){
+function animaciontotal(){
   var uno = document.getElementById('iniciar') ; 
  var el = document.getElementById('tren');    
   el.classList.toggle('pausa');
-if (uno.innerHTML == 'iniciar'){
- uno.innerHTML = 'pausar';
+if (uno.innerHTML == 'Iniciar Animacion'||uno.innerHTML == 'Reanudar Animacion'){
+ uno.innerHTML = 'Pausar Animacion';
+
  e1.style.WebkitAnimationPlayState = "running";
  e1.style.animationPlayState = 'running';
 } 
      
-  else {uno.innerHTML = 'iniciar'; 
+  else {uno.innerHTML = 'Reanudar Animacion'; 
+
  e1.style.WebkitAnimationPlayState = "paused";
  e1.style.animationPlayState = 'paused';
 }
-
 }
