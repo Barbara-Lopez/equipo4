69c9c6c6f1500f839dc715f40e5667d477781dac
Roberto <roberto.cerdan@ikasle.egibide.org>
2021-10-07 09:03:36 +0200

    cambios en gestion señales

--- html, css/js/gestion.js
+++ html, css/js/gestion.js
@@ -8,36 +8,15 @@ setInterval(function(){
 
 function actualizarVariables(){ //retorna un array 
     $.get("../portfolio/leer_variables.html", function(result) {
+		console.log(result)
             let variables = new Array()
             datos = result.split("|")
+			senal_bombilla = false
             for(i = 0; i < datos.length-1; i= i+2){
-                variable = new Map()
-                if(["BBERDE", "BROJAS", "BAMARILLO"].includes(datos[i])){
-                    variable.set("nombre-variable", "semaforo")
-                    switch(datos[i]){
-                        case "BBERDE":
-                            if(datos[i+1]=="1"){
-                                variable.set("valor", 1) // 1 para verde (ok), 2 para naranja(danger), 0 para rojo( no ok)
-                            }
-                            break
-                        case "BAMARILLO":
-                            if(datos[i+1]=="1"){
-                                variable.set("valor", 2) 
-                            }
-                            break
-                        case "BROJAS":
-                            if(datos[i+1]=="1"){
-                                variable.set("valor", 0) 
-                            }
-                            break
-                    }
-                    variables.push(variable)    
-                }
-                else{
-                    variable.set("nombre-variable", datos[i])
-                    variable.set("valor", datos[i+1])
-                    variables.push(variable)
-                }
+                variable = new Map()  
+				variable.set("nombre-variable", datos[i])
+				variable.set("valor", datos[i+1])
+				variables.push(variable)
             }
             actualizarVisu(variables)
 
@@ -48,34 +27,57 @@ function actualizarVariables(){ //retorna un array
 function actualizarVisu(variables){
     variables.forEach( variable => {
         nombre = "piloto-" + variable.get("nombre-variable")
-        piloto = document.getElementById(nombre.toLowerCase())
-        switch(variable.get("valor")){
-            case 0:
-                piloto.className="status noOk"
-                break
-            case 1:
-                piloto.className="status ok"
-                break
-            case 2:
-                piloto.className="status danger"
-        }
+		nombre = nombre.toLowerCase()
+        piloto = document.getElementById(nombre)
+		if(nombre != "piloto-semaforo"){
+			switch(variable.get("valor")){
+				case "0":
+					piloto.className="status noOk"
+					break
+				case "1":
+					piloto.className="status ok"
+					break
+			}
+		}
+		else{
+			switch(variable.get("valor")){
+				case "0":
+					piloto.className="status noStatus"
+					break
+				case "1":
+					piloto.className="status ok"
+					break
+				case "2":
+					piloto.className="status danger"
+					break
+				case "3":
+					piloto.className="status noOk"
+					break
+			}
+		}
     })
 }
 
 //tripleeeee
 function enviarPulsos(boton){
-    boton.value = 1
+	var formulario = document.createElement("form")
+	formulario.setAttribute("id", "formVirtual")
+	formulario.setAttribute("method", "post")
+	senal =  document.createElement("input")
+	senal.setAttribute('name', boton.name)
+	senal.setAttribute("type", "hidden")
+	senal.setAttribute("value", 1)
+	formulario.innerHTML = senal.outerHTML
+	console.log(formulario)
+
+
     $.ajax({
         type: "POST",
-        url: $("#form").attr('action'),
-        data: $boton.serialize()
-    })
-    boton.value = 0
-    $.ajax({
-        type: "POST",
-        url: $("#form").attr('action'),
-        data: $boton.serialize()
-    })
+        url: $('#form-controles').attr('action'),
+        data: $("#formVirtual").serialize()
+	})
+	
+    
 }
 
 function enclavamientoManualAutomatico(boton){ //se espera que el parametro sea el propio boton que genera la llamada
@@ -90,7 +92,8 @@ function enclavamientoManualAutomatico(boton){ //se espera que el parametro sea 
 
 /*envio de datos a plc INCLUIDOS CHECKBOXES A FALSE*/
 
-function enviarDatos(){
+function enviarDatos(boton){
+	enclavamientoManualAutomatico(boton)
     var $form = $('#form-controles').clone();
 
     //find the checkboxes
