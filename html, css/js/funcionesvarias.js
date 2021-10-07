d788cddb5a8751c7c4f6325ffcfb6efb8a81f9a9
unai <unai.diez@ikasle.egibide.org>
2021-10-07 09:20:52 +0200

    animacion del despliegue del menu v1.1
    retoques extras

--- html, css/js/funcionesvarias.js
+++ html, css/js/funcionesvarias.js
@@ -18,18 +18,20 @@ burguerIcon.addEventListener('click',() =>
     if(!botoncito.style.display)
     botoncito.style.display ="none"
     console.log(botoncito.style.display)
+
+
     if(botoncito.style.display == "none" || botoncito.style.overflow=="hidden")
     {
         if(mobileReso.matches)
         {
 
           botoncito.animate([{width : "0%" , display: "none"},{width : "100%" , display : "inherit"}],{duration: 200 , iterations:1, easing: "ease-in", fill: "forwards"})
-           botoncito.style.overflow="visible";
-              console.log("grando "+document.getElementById("sidenav"))
+          botoncito.style.overflow="visible";
+          console.log("grando "+document.getElementById("sidenav"))
         }
         else
         {
-            document.getElementById("main").animate
+          document.getElementById("main").animate
                 (
                 [{width :"100%"},{width :"80%"}],
                 {duration:200, iterations:1, easing: "ease-in", fill: "forwards" }
@@ -39,49 +41,44 @@ burguerIcon.addEventListener('click',() =>
                 [{width :"0%"  , display: "none"},{width : "20%",display : "inherit" }],
                 {duration: 200 , iterations:1, easing: "ease-in", fill: "forwards" }
                 )
-            botoncito.style.overflow="visible";
-            document.getElementById("sidenav").style.width = "20%";
-            document.getElementById("sidenav").style.display = "inherit";
-            document.getElementById("main").style.width = "80%";
-            document.getElementById("main").style.paddingLeft = "1em";
-           
-            
-              console.log("mini "+ botoncito.style)
+          botoncito.style.overflow="visible";
+          document.getElementById("sidenav").style.width = "20%";
+          document.getElementById("sidenav").style.display = "inherit";
+          document.getElementById("main").style.width = "80%";
+          document.getElementById("main").style.paddingLeft = "1em";
+          console.log("mini "+ botoncito.style)
         }
     }
-    else{
-        if(botoncito.width == "100%"){
-            botoncito.classList.toggle('aparecergrande')
-            
-             botoncito.animate([{width : "100%" , display : "inherit"},{width : "0%" , display: "none"}],{duration: 200 , iterations:1 , easing: "ease-in", fill: "forwards"})
-             botoncito.style.width = "0";
-             botoncito.style.display = "none";
-             document.getElementById("main").animate
-                (
-                [{width :"80%"},{width :"100%"}],
-                {duration:200, iterations:1, easing: "ease-in", fill: "forwards" }
-                )
-                botoncito.style.overflow="hidden";
+    else //animacion de vuelta al modo original
+    {
+        if(botoncito.width == "100%")
+        {
+         botoncito.animate([{width : "100%" , display : "inherit"},{width : "0%" , display: "none"}],{duration: 200 , iterations:1 , easing: "ease-in", fill: "forwards"})
+         botoncito.style.width = "0";
+         botoncito.style.display = "none";
+         document.getElementById("main").animate
+            (
+            [{width :"80%"},{width :"100%"}],
+            {duration:200, iterations:1, easing: "ease-in", fill: "forwards" }
+            )
+            botoncito.style.overflow="hidden";
         document.getElementById("main").style.width = "100%";
         document.getElementById("main").style.paddingLeft = "3em";
-        //botoncito.animate([{},{display :"none"}],{duration:200, iterations:1})
-        }
-        else{
-            document.getElementById("main").animate
-                (
-                [{width :"80%"},{width :"100%"}],
-                {duration:200, iterations:1, easing: "ease-in", fill: "forwards" }
-                )
             
-         botoncito.animate([{width : "20%" , display : "inherit"},{width : "0%"}],{duration: 200 , iterations:1, easing: "ease-in", fill: "forwards"})
-             botoncito.style.width = "0";
-            botoncito.style.overflow="hidden";
-             botoncito.classList.toggle('aparecermini')
-        document.getElementById("main").style.width = "100%";
-       document.getElementById("main").style.paddingLeft = "3em";
-       // botoncito.animate([{width :"20%",display:"inherit"},{width :"0%",display :"none"}],{duration:200, iterations:2})
-        
-            }
+        }
+        else
+        {
+          document.getElementById("main").animate
+            (
+            [{width :"80%"},{width :"100%"}],
+            {duration:200, iterations:1, easing: "ease-in", fill: "forwards" }
+            )        
+          botoncito.animate([{width : "20%" , display : "inherit"},{width : "0%"}],{duration: 200 , iterations:1, easing: "ease-in", fill: "forwards"})
+          botoncito.style.width = "0";
+          botoncito.style.overflow="hidden";
+          document.getElementById("main").style.width = "100%";
+          document.getElementById("main").style.paddingLeft = "3em";
+        }
     } 
 }
 , false)
@@ -92,43 +89,165 @@ return (100 * valornormal) / valor100;
 /*funcion de la animacion del tren automatico*/
 function animaciontotal()
 {
-  tren.classList.toggle('pausa');/*en caso de que tenga esa clase lo quita, sino lo implementa*/
-if (uno.innerHTML == 'Iniciar Animacion'||uno.innerHTML == 'Reanudar Animacion')
+  if(tren.value != 1){
+  moveraparada(1)
+  tren.value = 1
+  }
+  else
   {
-    if (uno.innerHTML == 'Iniciar Animacion') 
+    tren.classList.toggle('pausa');/*en caso de que tenga esa clase lo quita, sino lo implementa*/
+  if (uno.innerHTML == 'Iniciar Animacion'||uno.innerHTML == 'Reanudar Animacion')
     {
-    tren.classList.toggle('inicio')
-    tren.classList.toggle('pausa');
+      if (uno.innerHTML == 'Iniciar Animacion') 
+      {
+      tren.classList.toggle('inicio')
+      tren.classList.toggle('pausa');
+      }
+     uno.innerHTML = 'Pausar Animacion';
+    }      
+    else 
+    {
+    uno.innerHTML = 'Reanudar Animacion'; 
     }
-   uno.innerHTML = 'Pausar Animacion';
-  }      
-  else 
-  {
-  uno.innerHTML = 'Reanudar Animacion'; 
   }
 }
+
+
 /*funcion de la animacion del tren manual*/
-function moveraparada(destino){
+
+
+function moveraparada(destino)
+{
   let diferencia=0
   let cambio="";
   let trenecito =parseInt(tren.value);
       console.log(trenecito)
-   if (trenecito<destino) {
-    cambio="derecha";   
-    diferencia= parseInt(destino-trenecito)
-        moverderecha(destino,diferencia)
-    document.getElementById('tren').value=destino
+   if (trenecito<destino) 
+   {
+      cambio="derecha";   
+      diferencia= parseInt(destino-trenecito)
+      moverderecha(destino,diferencia)/*se va a la funcion de mover a la derecha*/
+      document.getElementById('tren').value=destino
 
    }else  
-   if (trenecito>destino) {
-    cambio="izquierda";   
-    diferencia= parseInt(trenecito-destino)
-    moverizquierda(destino,diferencia)
-    document.getElementById('tren').value=destino
+   if (trenecito>destino) 
+   {
+      cambio="izquierda";   
+      diferencia= parseInt(trenecito-destino)
+      moverizquierda(destino,diferencia)/*se va a la funcion de mover a la izquierda*/
+      document.getElementById('tren').value=destino
    }
-    
     console.log(destino + cambio + diferencia)
 }
+
+
+
+//animacion manual (de un lado a otro directamente) 
+function moverizquierda(destino,cantidad)
+{    
+  console.log(tren.value+","+destino)
+  var idniani =porcentage((30*(tren.value-1)),100)+"%"
+  var finiani=porcentage((30*(tren.value-1))-(30*cantidad),100)+"%"
+  var tiempo=1000*cantidad
+  console.log(idniani+","+finiani)
+  document.getElementById('tren').animate
+  (
+    [ 
+      { left: idniani }, 
+      { left: finiani }
+    ], 
+    {
+    duration: tiempo,
+    iterations: 1
+    }
+  )
+  tren.style.left = finiani;
+}
+
+function moverderecha(destino,cantidad)
+{
+  console.log(tren.value+","+destino)
+  var idniani=porcentage((30*(tren.value-1)),100)+"%"
+  var finiani=porcentage((30*(tren.value-1))+(30*cantidad),100)+"%"
+  var tiempo=1000*cantidad
+  console.log(idniani+","+finiani)
+  document.getElementById('tren').animate
+(
+    [ 
+      { left: idniani }, 
+      { left: finiani }
+    ], 
+    {
+    duration: tiempo,
+    iterations: 1
+    }
+  )
+  tren.style.left = finiani;
+}
+
+
+/* funciones de usuario y contraseña */
+
+
+function validarUsuario() 
+{
+    let nombre=document. getElementById ("user").value;
+        regNombre=new RegExp("^admin$") 
+    let passwd=document. getElementById ('passwd').value;
+        regPasswd=new RegExp("^12345$")
+    if (regNombre.test(nombre) && regPasswd.test(passwd)) 
+    {
+       window.location="gestion.html";
+    }
+    else 
+        swal("Usuario o contraseña no validos", {icon: "error",})
+}
+
+
+/* funciones Contacto */
+
+
+function enviarContacto()
+{
+    let nombre=document.getElementById('nombre').value;
+    let correo=document.getElementById('correo').value;
+    let asunto=document.getElementById('asunto').value;
+    let descripcion=document.getElementById('descripcion').value;
+    let regNombre=new RegExp("^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$")
+    console.log(nombre.length)
+    if(nombre.length==0 || correo.length==0 || asunto.length==0 || descripcion.length==0 )
+    {
+        swal("No se puede dejar ningun campo vacio", {icon: "info",})
+    }
+    else
+    {
+        if (!regNombre.test(correo))
+        {
+            swal("El apartado de correo electronico no tiene la estructura de un correo", {icon: "info",})
+        }
+        else
+        {
+            swal({
+                title: "Estas seguro?",
+                text: "Una vez que le des a 'OK' se enviará",
+                icon: "warning",
+                buttons: true,
+                dangerMode: true,
+            })
+            .then((willDelete) => 
+            {
+                if (willDelete) 
+                {
+                swal("Se ha enviado correctamente",{ 
+                  icon: "success",
+                  });
+                } 
+            });
+        }
+    }
+}
+
+
 /* borrador 1 (de un lado a otro parandose en cada parada)*/
 /*
 function moverizquierda(destino,cantidad){
@@ -184,100 +303,5 @@ function moverderecha(destino,cantidad){
         console.log(document.getElementById('tren').style.left)
         console.log(document.getElementById('tren').style.value)
   }
-}*/
-//animacion correcta v1 (de un lado a otro directamente)
-function moverizquierda(destino,cantidad)
-{    
-  console.log(tren.value+","+destino)
-  var idniani =porcentage((30*(tren.value-1)),100)+"%"
-  var finiani=porcentage((30*(tren.value-1))-(30*cantidad),100)+"%"
-  var tiempo=1000*cantidad
-  console.log(idniani+","+finiani)
-  document.getElementById('tren').animate
-  (
-    [ 
-      { left: idniani }, 
-      { left: finiani }
-    ], 
-    {
-    duration: tiempo,
-    iterations: 1
-    }
-  )
-  tren.style.left = finiani;
 }
-
-function moverderecha(destino,cantidad)
-{
-  console.log(tren.value+","+destino)
-  var idniani=porcentage((30*(tren.value-1)),100)+"%"
-  var finiani=porcentage((30*(tren.value-1))+(30*cantidad),100)+"%"
-  var tiempo=1000*cantidad
-  console.log(idniani+","+finiani)
-  document.getElementById('tren').animate
-(
-    [ 
-      { left: idniani }, 
-      { left: finiani }
-    ], 
-    {
-    duration: tiempo,
-    iterations: 1
-    }
-  )
-  tren.style.left = finiani;
-}
-/* usuario y contraseña */
-
-
-function validarUsuario() {
-    let nombre=document. getElementById ("user").value;
-    regNombre=new RegExp("^admin$") 
-    let passwd=document. getElementById ('passwd').value;
-    regPasswd=new RegExp("^12345$")
-    if (regNombre.test(nombre) && regPasswd.test(passwd)) 
-    {
-       window.location="gestion.html";
-    }
-    else 
-        swal("Usuario o contraseña no validos", {icon: "error",})
-}
-
-
-
-
-
-/* funciones Contacto */
-function enviarContacto(){
-    let nombre=document.getElementById('nombre').value;
-    let correo=document.getElementById('correo').value;
-    let asunto=document.getElementById('asunto').value;
-    let descripcion=document.getElementById('descripcion').value;
-    let regNombre=new RegExp("^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$")
-    console.log(nombre.length)
-    if(nombre.length==0 || correo.length==0 || asunto.length==0 || descripcion.length==0 ){
-        swal("No se puede dejar ningun campo vacio", {icon: "info",})
-    }
-    else{
-        
-        if (!regNombre.test(correo)){
-            swal("El apartado de correo electronico no tiene la estructura de un correo", {icon: "info",})
-        }
-        else{
-            swal({
-                title: "Estas seguro?",
-                text: "Una vez que le des a 'OK' se enviará",
-                icon: "warning",
-                buttons: true,
-                dangerMode: true,
-            })
-            .then((willDelete) => {
-                if (willDelete) {
-                swal("Se ha enviado correctamente", {
-                    icon: "success",
-                });
-                } 
-            });
-        }
-    }
- }
+*/
